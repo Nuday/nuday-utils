017bc3bfaa780a9db0da8f8bad8c4f38d950d489
Josh Glover <jmglov@gmail.com>
2015-02-11 11:33:07 +0100

    types : add Map

--- src/nuday/utils/types.clj
+++ src/nuday/utils/types.clj
@@ -1,8 +1,10 @@
 (ns nuday.utils.types
-  (:require [schema.core :as s]))
+  (:require [schema.core :as s :refer [Any Keyword]]))
 
-(def ^:private uuid?
-  (partial re-matches #"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"))
+(declare uuid?)
+
+(def Map
+  {Keyword Any})
 
 (def NonEmptyStr
   (s/both s/Str 
@@ -11,3 +13,6 @@
 (def UuidStr
   (s/both s/Str
           (s/pred uuid? 'uuid?)))
+
+(def ^:private uuid?
+  (partial re-matches #"[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"))
