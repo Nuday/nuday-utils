a0276d35345c9599d90a854c3e9c68e2e25cf08a
Yury Yantsevich <featalion@gmail.com>
2015-09-29 22:48:05 +0300

    clojure 1.7

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/Nuday/nuday-utils"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [environ "1.0.0"]
                  [prismatic/schema "0.4.4"]]
 
-  :plugins [[org.clojars.cvillecsteele/lein-git-version "1.0.3"]]
+  :plugins [[org.clojars.cvillecsteele/lein-git-version "1.0.2"]]
 
   :profiles {:dev {:dependencies [[pjstadig/humane-test-output "0.6.0"]]
                    :injections [(require 'pjstadig.humane-test-output)
