1e596812a066219144a97af6ca413575cb7ee522
Yury Yantsevich <featalion@gmail.com>
2016-04-01 19:02:57 +0300

    clojure and dependencies update

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/Nuday/nuday-utils"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
-                 [environ "1.0.0"]
-                 [prismatic/schema "1.0.1"]]
+                 [environ "1.0.2"]
+                 [prismatic/schema "1.1.0"]]
 
-  :plugins [[org.clojars.cvillecsteele/lein-git-version "1.0.2"]]
+  :plugins [[org.clojars.cvillecsteele/lein-git-version "1.0.3"]]
 
   :profiles {:dev {:dependencies [[pjstadig/humane-test-output "0.6.0"]]
                    :injections [(require 'pjstadig.humane-test-output)
